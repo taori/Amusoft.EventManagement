7418f3277e0db06fb67fe16723cc361074b11f5e
Andreas Müller <taori@users.noreply.github.com>
2018-08-13 13:36:46 +0200

    License header fixes

--- tests/Amusoft.EventManagement.Test/TestEnvironment/EventReceiver/AsyncEventHandlerReceiver.cs
+++ tests/Amusoft.EventManagement.Test/TestEnvironment/EventReceiver/AsyncEventHandlerReceiver.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright 2018 Andreas Müller
-// This file is a part of Amusoft and is licensed under MIT
-// See https://github.com/taori/Amusoft/blob/master/LICENSE.MD for details
+// This file is a part of Amusoft and is licensed under Apache 2.0
+// See https://github.com/taori/Amusoft.EventManagement/blob/master/LICENSE.MD for details
 
 namespace Amusoft.EventManagement.Test.TestEnvironment.EventReceiver
 {
